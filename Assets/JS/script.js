58db21cc22b6c9a57d3bfdb8f8924acb2e2bd30e
Bonnie Reed <bonniejeanreed@gmail.com>
2021-09-20 14:35:40 -0500

    added modals to larger screens to search from navbar.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,23 +1,25 @@
-nearbyButtonEl = document.getElementById('nearbyBtn');
-byCityButtonEl = document.getElementById('byZipBtn');
-nearbyButtonDisplayEl = document.getElementById('nearby-form');
+nearbyButtonEl = document.getElementById("nearbyBtn");
+byCityButtonEl = document.getElementById("byZipBtn");
+nearbyButtonDisplayEl = document.getElementById("nearby-form");
 
-
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems, {});
+
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems, {});
 });
 
 // Grabs first 20 and lists out name and URL
 function urlList() {
-  var requestUrl = 'https://api.openbrewerydb.org/breweries?per_page=15';
+  var requestUrl = "https://api.openbrewerydb.org/breweries?per_page=15";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log('Brewery Name and Website \n----------');
+      console.log("Brewery Name and Website \n----------");
       for (var i = 0; i < data.length; i++) {
         console.log(data[i].website_url);
         console.log(data[i].name);
@@ -27,14 +29,14 @@ function urlList() {
 
 // This function will take in user Zip input and concat it onto the API link and sort breweries by relevant zip code
 function findByZip(userZip) {
-  var byZip = 'https://api.openbrewerydb.org/breweries?by_postal=' + userZip;
+  var byZip = "https://api.openbrewerydb.org/breweries?by_postal=" + userZip;
 
   fetch(byZip)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log('Brewery Name and ZipCode \n----------');
+      console.log("Brewery Name and ZipCode \n----------");
       for (var i = 0; i < data.length; i++) {
         console.log(data[i].postal_code);
         console.log(data[i].name);
@@ -42,24 +44,22 @@ function findByZip(userZip) {
     });
 }
 
-
-//This function will take in user city input and concat it onto the API link and sort breweries by relevant city 
+//This function will take in user city input and concat it onto the API link and sort breweries by relevant city
 function findByCity(userCity) {
-  var byCity = 'https://api.openbrewerydb.org/breweries?by_city=' + userCity;
+  var byCity = "https://api.openbrewerydb.org/breweries?by_city=" + userCity;
 
   fetch(byCity)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log('Brewery Name and ZipCode \n----------');
+      console.log("Brewery Name and ZipCode \n----------");
       for (var i = 0; i < data.length; i++) {
         console.log(data[i].city);
         console.log(data[i].name);
       }
     });
 }
-
 
 //function gets users current location
 function getLocation() {
@@ -72,21 +72,26 @@ function getLocation() {
 
 //The function displays the Lat and Longitude for users position
 function showPosition(position) {
-  console.log("Latitude: " + position.coords.latitude +
-    " Longitude: " + position.coords.longitude);
+  console.log(
+    "Latitude: " +
+      position.coords.latitude +
+      " Longitude: " +
+      position.coords.longitude
+  );
   console.log("I've got your position!");
 }
 
-// Prompts user to allow or deny location access if allowed reveals nearby button 
-navigator.geolocation.watchPosition(function (position) {
-  getLocation();
-  nearbyButtonDisplayEl.classList.remove('hide')
-},
+// Prompts user to allow or deny location access if allowed reveals nearby button
+navigator.geolocation.watchPosition(
+  function (position) {
+    getLocation();
+    nearbyButtonDisplayEl.classList.remove("hide");
+  },
 
-// If permission denied hides nearby button
+  // If permission denied hides nearby button
   function (error) {
     if (error.code == error.PERMISSION_DENIED)
-      nearbyButtonDisplayEl.classList.add('hide');
+      nearbyButtonDisplayEl.classList.add("hide");
     console.log("you denied location access");
-  });
-
+  }
+);
